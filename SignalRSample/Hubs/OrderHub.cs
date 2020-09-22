d79943c3dc1dfcbf5218ce5127d417da50d76900
Feyyaz Acet <feyyaz.acet@tetris-tr.com>
2020-09-23 00:18:00 +0300

    sample signalR project

---
+++ SignalRSample/Hubs/OrderHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Hubs
+{
+    public class OrderHub : Hub
+    {
+     
+        public override Task OnConnectedAsync()
+        {
+            //get conntection Id Context.ConnectionId;
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
